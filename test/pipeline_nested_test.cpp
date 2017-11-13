89c7d3d70917cdb0cf3e4fb4430b9648ecd37732
danpaol <dan.floyd111@gmail.com>
2017-11-13 16:16:10 +0100

    Added nested pipeline test case to the test suite

---
+++ test/pipeline_nested_test.cpp
@@ -0,0 +1,85 @@
+/*
+ *  Author: Daniele Paolini, daniele.paolini@hotmail.it
+ * 
+ *  Nested pipeline test:
+ *  Composing two nodes with a pipeline in between and one more pipeline nested into the previous one
+ *  Comp(Node1, Pipeline, Node2) where Pipeline = [Node3 -> InnerPipeline -> Node4] and
+ *  InnerPipeline = [Increment -> Double], Node-i will add i to his input (except for node 1)
+ *  Expected: Node2(Node4(Double(Increment(Node3(Node1(x)))))) where x is the input
+ * 
+*/
+
+#include <cassert>
+#include <iostream>
+#include "../comp.hpp"
+
+using namespace ff;
+using namespace std;
+
+struct Node1: ff_node {
+    void* svc(void *t){
+        if (t) return t;
+        return new int(42);
+    }
+};
+
+struct Node2: ff_node {
+    void* svc(void *t){
+        *((int*)t)+=2;
+        return t;
+    }
+};
+
+struct Node3: ff_node {
+    void* svc(void *t){
+        *((int*)t)+=3;
+        return t;
+    }
+};
+
+struct Node4: ff_node {
+    void* svc(void *t){
+        *((int*)t)+=4;
+        return t;
+    }
+};
+
+struct Incr: ff_node {
+    void* svc(void *t){
+        *((int*)t)+=1;
+        return t;
+    }
+};
+
+struct Doub: ff_node {
+    void* svc(void *t){
+        *((int*)t)*=2;
+        return t;
+    }
+};
+
+int main() {
+    Node1 node1;
+    Node2 node2;
+    Node3 node3;
+    Node4 node4;
+    Incr incr;
+    Doub doub;
+    ff_pipeline pipeline, inner_pipeline;
+    ff_comp comp;
+    inner_pipeline.add_stage(&incr);
+    inner_pipeline.add_stage(&doub);
+    pipeline.add_stage(&node3);
+    pipeline.add_stage(&inner_pipeline);
+    pipeline.add_stage(&node4);
+    comp.add_stage(&node1);
+    comp.add_stage(&pipeline);
+    comp.add_stage(&node2);
+    cout << "Executing nested pipelines test without input..." << endl;
+    assert(*((int*)comp.run())==98);
+    cout << "-> PASSED" << endl;
+    cout << "Executing nested pipelines test with input..." << endl;
+    assert(*((int*)comp.run(new int(2)))==18);
+    cout << "-> PASSED" << endl;
+    return EXIT_SUCCESS;
+}
