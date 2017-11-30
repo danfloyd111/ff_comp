cb26be6bc8a4a92855ec61aa3fdfa6db76a68224
danpaol <dan.floyd111@gmail.com>
2017-11-30 14:41:18 +0100

    Did a bit refactoring

--- comp.hpp
+++ comp.hpp
@@ -20,8 +20,6 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- *
  ****************************************************************************
  */
 
@@ -38,7 +36,7 @@ namespace ff {
     class ff_comp: public ff_node {
 
     private:
-        svector<ff_node *> nodes_list;
+        svector<ff_node *> nodes;
         svector<ff_node *> decompose(ff_node* node);
         std::chrono::time_point<std::chrono::system_clock> cstart;
         std::chrono::time_point<std::chrono::system_clock> cend;
@@ -53,7 +51,7 @@ namespace ff {
         ff_comp() = default;
         ~ff_comp() = default; 
         int add_stage(ff_node *stage);
-        const svector<ff_node *>& get_stages() const { return nodes_list; };
+        const svector<ff_node *>& get_stages() const { return nodes; };
          // init task is the inital task submitted to comp, ex: f(g(h(init_task))), if init_task is null h (in this example) is a function that
          // takes no input (emitter, constant function, ...)
         void *run(void *init_task=nullptr);
@@ -64,7 +62,7 @@ namespace ff {
     int ff_comp::add_stage(ff_node *stage) {
         if (!stage) return -1;
         svector<ff_node *> nested = decompose(stage);
-        for (ff_node *n : nested) nodes_list.push_back(n);
+        for (ff_node *n : nested) nodes.push_back(n);
         return 0;
     }
 
@@ -72,10 +70,10 @@ namespace ff {
         
         cstart = std::chrono::system_clock::now();
         void *_in=nullptr, *_out=nullptr;
-        if (nodes_list.empty()) error("comp has no stages to execute\n");
-        for(size_t i=0; i<nodes_list.size(); ++i) {
-            if (i == 0) _out = nodes_list[i]->svc(init_task); // first call
-            else _out = nodes_list[i]->svc(_in);
+        if (nodes.empty()) error("comp has no stages to execute\n");
+        for(size_t i=0; i<nodes.size(); ++i) {
+            if (i == 0) _out = nodes[i]->svc(init_task); // first call
+            else _out = nodes[i]->svc(_in);
             _in = _out;
         }
         cend = std::chrono::system_clock::now();
