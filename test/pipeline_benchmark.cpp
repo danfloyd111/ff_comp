d5615fd1ffb59afeada6d806192242de5fafaf85
danpaol <dan.floyd111@gmail.com>
2017-12-01 19:28:07 +0100

    Completed sequential test

--- test/pipeline_benchmark.cpp
+++ test/pipeline_benchmark.cpp
@@ -10,18 +10,24 @@
 */
 
 #include <iostream>
-#include <fstream>
 #include <random>
 #include <functional>
 #include <vector>
+#include <cmath>
+#include <chrono>
 
 using namespace std;
+
+// helper functions (definitions are at the bottom of this file)
+double seq_sin(double);
+double seq_cos(double);
+double seq_fun(double);
 
 int main() {
 
     // creating an universal random big data set for the benchmark (~50MB)
-    const long DATA_SIZE = 6553600;
-    const string DATA_PATH = "test/bin/pipeline_benchmark_dataset"; 
+    
+    const size_t DATA_SIZE = 6553600; 
 
     uniform_real_distribution<double> dist {1, 10000};
     default_random_engine engine { };
@@ -29,31 +35,50 @@ int main() {
     
     cout << "Generating random data set..." << endl;
 
-    ofstream data_out;
-    data_out.open(DATA_PATH, ios::out);
-    if(!data_out.is_open()) {
-        cerr << "ERROR: there was a problem opening the output file" << endl;
-        return EXIT_FAILURE;
-    }
-    for (auto i=0; i<DATA_SIZE; ++i) data_out << next_value() << "\n";
-    data_out.close();
+    vector<double> data_set;
+    data_set.reserve(DATA_SIZE);
+    for (auto i=0; i<DATA_SIZE; ++i) data_set.push_back(next_value());
 
     cout << "Done!" << endl;
     
-    // beginning of the completely sequential test
-    vector<double> data;
-    data.reserve(DATA_SIZE);
-    ifstream data_in;
-    data_in.open(DATA_PATH, ios::in);
-    if(!data_in.is_open()) {
-        cerr << "ERROR: there was a problem opening the input file" << endl;
-        return EXIT_FAILURE;
+    // sequential test
+    
+    vector<double> seq_result_set;
+    std::chrono::time_point<std::chrono::system_clock> seq_start;
+    std::chrono::time_point<std::chrono::system_clock> seq_end;
+    seq_result_set.reserve(DATA_SIZE);
+    cout << "Beginning sequential computation..." << endl;
+    seq_start = chrono::system_clock::now();
+    for (auto i=0; i<DATA_SIZE; ++i) {
+        auto tmp = seq_sin(data_set[i]); // stage 1
+        tmp = seq_cos(tmp);              // stage 2
+        tmp = sqrt(tmp);                 // stage 3
+        tmp = seq_fun(tmp);              // stage 4
+        tmp = seq_sin(tmp);              // stage 5
+        tmp = seq_cos(tmp);              // stage 6
+        tmp = atan(tmp*tmp);             // stage 7
+        seq_result_set.push_back(tmp);
     }
-    double tmp;
-    while (data_in >> tmp) data.push_back(tmp);
-    data_in.close();
+    seq_end = chrono::system_clock::now();
+    cout << "Done! [Elapsed time: " << ((std::chrono::duration<double, std::milli>) (seq_end-seq_start)).count() << "(ms)]" << endl;
 
-    cout << data.size() << endl;
     return EXIT_SUCCESS;
 
+}
+
+
+// definition of the helper functions
+
+double seq_sin(double x) {
+    for (auto i=0; i<10; ++i) x = sin(x);
+    return x;
+}
+
+double seq_cos(double x) {
+    for (auto i=0; i<10; ++i) x = cos(x);
+    return x;
+}
+
+double seq_fun(double x) {
+    return log(2*M_PI*x);
 }
